8cc9ff9b3948d1150e440835d0334edec6885d53
ofilipelourencodesigner-cloud <ofilipelourencodesigner@gmail.com>
2025-10-01 10:19:14 -0300

    Add files via upload

---
+++ rotas.js
@@ -0,0 +1,74 @@
+// Rota 1 real + 4 fictícias
+const ROTAS = [
+  {
+    id: 1,
+    nome: "Green Valley",
+    pontos: [
+      { ordem: 1,  nome: "P1 - 6h10 | Cantinho da Viola",                                   latitude: -22.960993547862227, longitude: -48.40401957175889 },
+      { ordem: 2,  nome: "P2 - Ponto Onibus - Condomínio Alvorada",                         latitude: -22.955992342723256, longitude: -48.41731646486665 },
+      { ordem: 3,  nome: "P3 - Recuo Ecoterapia na Gastão Dal Farra",                       latitude: -22.954214442857754, longitude: -48.421357467013394 },
+      { ordem: 4,  nome: "P4 - Green Valley",                                               latitude: -22.94969387238644,  longitude: -48.427186919900265 },
+      { ordem: 5,  nome: "P5 - 6h20 | Green Valley",                                        latitude: -22.949569164150102, longitude: -48.42572500263284 },
+      { ordem: 6,  nome: "P6 - Aluna com deficiência - Precisa de auxílio | Green Valley",  latitude: -22.950490726474047, longitude: -48.425551703449244 },
+      { ordem: 7,  nome: "P7 - Green Valley",                                               latitude: -22.952525054857322, longitude: -48.42551392669989 },
+      { ordem: 8,  nome: "E1 - Elda | CTEEP Subestação energia",                            latitude: -22.949677609533758, longitude: -48.43859474586661 },
+      { ordem: 9,  nome: "E2 - 6h35 | Rafaela Benato | 24 de Maio",                         latitude: -22.92262373666585,  longitude: -48.44942925206166 },
+      { ordem: 10, nome: "E3 - Sophia Gabriel | COHAB 1",                                   latitude: -22.916377074744066, longitude: -48.44122118000235 },
+      { ordem: 11, nome: "E4 - Serrinha | COHAB 1",                                         latitude: -22.914004270202742, longitude: -48.43688103006206 },
+      { ordem: 12, nome: "E5 - APAE | YASMIN | Auxílio para descer e subir",                latitude: -22.893864869561845, longitude: -48.453956756789154 },
+      { ordem: 13, nome: "E6 - Américo",                                                    latitude: -22.897569531105284, longitude: -48.447882578279035 }
+    ]
+  },
+  {
+    id: 2, nome: "Bairro Central (fictícia)", pontos: [
+      { ordem:1,nome:"Ponto 1",latitude:-22.910,longitude:-48.400 },
+      { ordem:2,nome:"Ponto 2",latitude:-22.911,longitude:-48.402 },
+      { ordem:3,nome:"Ponto 3",latitude:-22.912,longitude:-48.404 },
+      { ordem:4,nome:"Ponto 4",latitude:-22.913,longitude:-48.406 },
+      { ordem:5,nome:"Ponto 5",latitude:-22.914,longitude:-48.408 },
+      { ordre:6,nome:"Ponto 6",latitude:-22.915,longitude:-48.410 },
+      { ordem:7,nome:"Ponto 7",latitude:-22.916,longitude:-48.412 },
+      { ordem:8,nome:"Ponto 8",latitude:-22.917,longitude:-48.414 },
+      { ordem:9,nome:"Ponto 9",latitude:-22.918,longitude:-48.416 },
+      { ordem:10,nome:"Ponto 10",latitude:-22.919,longitude:-48.418 }
+    ]
+  },
+  {
+    id: 3, nome: "Jardim das Flores (fictícia)", pontos: [
+      { ordem:1,nome:"Ponto 1",latitude:-22.920,longitude:-48.420 },
+      { ordem:2,nome:"Ponto 2",latitude:-22.921,longitude:-48.422 },
+      { ordem:3,nome:"Ponto 3",latitude:-22.922,longitude:-48.424 },
+      { ordem:4,nome:"Ponto 4",latitude:-22.923,longitude:-48.426 },
+      { ordem:5,nome:"Ponto 5",latitude:-22.924,longitude:-48.428 },
+      { ordem:6,nome:"Ponto 6",latitude:-22.925,longitude:-48.430 },
+      { ordem:7,nome:"Ponto 7",latitude:-22.926,longitude:-48.432 },
+      { ordem:8,nome:"Ponto 8",latitude:-22.927,longitude:-48.434 },
+      { ordem:9,nome:"Ponto 9",latitude:-22.928,longitude:-48.436 },
+      { ordem:10,nome:"Ponto 10",latitude:-22.929,longitude:-48.438 }
+    ]
+  },
+  { id: 4, nome: "Vila Nova (fictícia)", pontos: [
+      { ordem:1,nome:"Ponto 1",latitude:-22.930,longitude:-48.440 },
+      { ordem:2,nome:"Ponto 2",latitude:-22.931,longitude:-48.442 },
+      { ordem:3,nome:"Ponto 3",latitude:-22.932,longitude:-48.444 },
+      { ordem:4,nome:"Ponto 4",latitude:-22.933,longitude:-48.446 },
+      { ordem:5,nome:"Ponto 5",latitude:-22.934,longitude:-48.448 },
+      { ordem:6,nome:"Ponto 6",latitude:-22.935,longitude:-48.450 },
+      { ordem:7,nome:"Ponto 7",latitude:-22.936,longitude:-48.452 },
+      { ordem:8,nome:"Ponto 8",latitude:-22.937,longitude:-48.454 },
+      { ordem:9,nome:"Ponto 9",latitude:-22.938,longitude:-48.456 },
+      { ordem:10,nome:"Ponto 10",latitude:-22.939,longitude:-48.458 }
+  ]},
+  { id: 5, nome: "São José (fictícia)", pontos: [
+      { ordem:1,nome:"Ponto 1",latitude:-22.940,longitude:-48.460 },
+      { ordem:2,nome:"Ponto 2",latitude:-22.941,longitude:-48.462 },
+      { ordem:3,nome:"Ponto 3",latitude:-22.942,longitude:-48.464 },
+      { ordem:4,nome:"Ponto 4",latitude:-22.943,longitude:-48.466 },
+      { ordem:5,nome:"Ponto 5",latitude:-22.944,longitude:-48.468 },
+      { ordem:6,nome:"Ponto 6",latitude:-22.945,longitude:-48.470 },
+      { ordem:7,nome:"Ponto 7",latitude:-22.946,longitude:-48.472 },
+      { ordem:8,nome:"Ponto 8",latitude:-22.947,longitude:-48.474 },
+      { ordem:9,nome:"Ponto 9",latitude:-22.948,longitude:-48.476 },
+      { ordem:10,nome:"Ponto 10",latitude:-22.949,longitude:-48.478 }
+  ]}
+];
