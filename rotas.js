ea745923d77eb244900d1b9c632a409fe8341c37
ofilipelourencodesigner-cloud <ofilipelourencodesigner@gmail.com>
2025-10-02 11:27:56 -0300

    Update rotas.js

--- rotas.js
+++ rotas.js
@@ -1,74 +1,85 @@
-// Rota 1 real + 4 fictícias
-const ROTAS = [
+[
   {
-    id: 1,
-    nome: "Green Valley",
-    pontos: [
-      { ordem: 1,  nome: "P1 - 6h10 | Cantinho da Viola",                                   latitude: -22.960993547862227, longitude: -48.40401957175889 },
-      { ordem: 2,  nome: "P2 - Ponto Onibus - Condomínio Alvorada",                         latitude: -22.955992342723256, longitude: -48.41731646486665 },
-      { ordem: 3,  nome: "P3 - Recuo Ecoterapia na Gastão Dal Farra",                       latitude: -22.954214442857754, longitude: -48.421357467013394 },
-      { ordem: 4,  nome: "P4 - Green Valley",                                               latitude: -22.94969387238644,  longitude: -48.427186919900265 },
-      { ordem: 5,  nome: "P5 - 6h20 | Green Valley",                                        latitude: -22.949569164150102, longitude: -48.42572500263284 },
-      { ordem: 6,  nome: "P6 - Aluna com deficiência - Precisa de auxílio | Green Valley",  latitude: -22.950490726474047, longitude: -48.425551703449244 },
-      { ordem: 7,  nome: "P7 - Green Valley",                                               latitude: -22.952525054857322, longitude: -48.42551392669989 },
-      { ordem: 8,  nome: "E1 - Elda | CTEEP Subestação energia",                            latitude: -22.949677609533758, longitude: -48.43859474586661 },
-      { ordem: 9,  nome: "E2 - 6h35 | Rafaela Benato | 24 de Maio",                         latitude: -22.92262373666585,  longitude: -48.44942925206166 },
-      { ordem: 10, nome: "E3 - Sophia Gabriel | COHAB 1",                                   latitude: -22.916377074744066, longitude: -48.44122118000235 },
-      { ordem: 11, nome: "E4 - Serrinha | COHAB 1",                                         latitude: -22.914004270202742, longitude: -48.43688103006206 },
-      { ordem: 12, nome: "E5 - APAE | YASMIN | Auxílio para descer e subir",                latitude: -22.893864869561845, longitude: -48.453956756789154 },
-      { ordem: 13, nome: "E6 - Américo",                                                    latitude: -22.897569531105284, longitude: -48.447882578279035 }
+    "id": 1,
+    "nome": "Green Valley",
+    "pontos": [
+      {"nome":"P1 - 6h10 | Cantinho da Viola","latitude":-22.960993547862227,"longitude":-48.40401957175889},
+      {"nome":"P2 - Ponto Ônibus - Cond. Alvorada","latitude":-22.955992342723256,"longitude":-48.417316464868665},
+      {"nome":"P3 - Recuo Ecoterapia na Gastão Dal Farra","latitude":-22.954214442857754,"longitude":-48.421357467013394},
+      {"nome":"P4 - Green Valley","latitude":-22.949689372873864,"longitude":-48.427186919900265},
+      {"nome":"P5 - 6h20 | Green Valley","latitude":-22.949569164150102,"longitude":-48.425725002063284},
+      {"nome":"P6 - Aluna com deficiência (auxílio) | Green Valley","latitude":-22.950490726474047,"longitude":-48.425551703449244},
+      {"nome":"P7 - Green Valley","latitude":-22.952525054857322,"longitude":-48.42551392669989},
+      {"nome":"E1 - Elda | CTEEP Subestação energia","latitude":-22.949677609533758,"longitude":-48.43859497458661},
+      {"nome":"E2 - 6h35 | Rafaela Benato | 24 de Maio","latitude":-22.92262373666585,"longitude":-48.444942925206166},
+      {"nome":"E3 - Sophia Gabriel | COHAB 1","latitude":-22.916377074744066,"longitude":-48.44122181000235},
+      {"nome":"E4 - Serrinha | COHAB 1","latitude":-22.941004270202742,"longitude":-48.43688103006206},
+      {"nome":"E5 - APAE | YASMIN (auxílio)","latitude":-22.893864869561845,"longitude":-48.453956756789154},
+      {"nome":"E6 - Américo","latitude":-22.897565931105284,"longitude":-48.447882578279035}
     ]
   },
   {
-    id: 2, nome: "Bairro Central (fictícia)", pontos: [
-      { ordem:1,nome:"Ponto 1",latitude:-22.910,longitude:-48.400 },
-      { ordem:2,nome:"Ponto 2",latitude:-22.911,longitude:-48.402 },
-      { ordem:3,nome:"Ponto 3",latitude:-22.912,longitude:-48.404 },
-      { ordem:4,nome:"Ponto 4",latitude:-22.913,longitude:-48.406 },
-      { ordem:5,nome:"Ponto 5",latitude:-22.914,longitude:-48.408 },
-      { ordre:6,nome:"Ponto 6",latitude:-22.915,longitude:-48.410 },
-      { ordem:7,nome:"Ponto 7",latitude:-22.916,longitude:-48.412 },
-      { ordem:8,nome:"Ponto 8",latitude:-22.917,longitude:-48.414 },
-      { ordem:9,nome:"Ponto 9",latitude:-22.918,longitude:-48.416 },
-      { ordem:10,nome:"Ponto 10",latitude:-22.919,longitude:-48.418 }
+    "id": 2,
+    "nome": "Bairro Central (fictícia)",
+    "pontos": [
+      {"nome":"1","latitude":-22.89,"longitude":-48.45},
+      {"nome":"2","latitude":-22.895,"longitude":-48.445},
+      {"nome":"3","latitude":-22.9,"longitude":-48.44},
+      {"nome":"4","latitude":-22.905,"longitude":-48.438},
+      {"nome":"5","latitude":-22.91,"longitude":-48.436},
+      {"nome":"6","latitude":-22.915,"longitude":-48.434},
+      {"nome":"7","latitude":-22.92,"longitude":-48.432},
+      {"nome":"8","latitude":-22.925,"longitude":-48.43},
+      {"nome":"9","latitude":-22.93,"longitude":-48.428},
+      {"nome":"10","latitude":-22.935,"longitude":-48.426}
     ]
   },
   {
-    id: 3, nome: "Jardim das Flores (fictícia)", pontos: [
-      { ordem:1,nome:"Ponto 1",latitude:-22.920,longitude:-48.420 },
-      { ordem:2,nome:"Ponto 2",latitude:-22.921,longitude:-48.422 },
-      { ordem:3,nome:"Ponto 3",latitude:-22.922,longitude:-48.424 },
-      { ordem:4,nome:"Ponto 4",latitude:-22.923,longitude:-48.426 },
-      { ordem:5,nome:"Ponto 5",latitude:-22.924,longitude:-48.428 },
-      { ordem:6,nome:"Ponto 6",latitude:-22.925,longitude:-48.430 },
-      { ordem:7,nome:"Ponto 7",latitude:-22.926,longitude:-48.432 },
-      { ordem:8,nome:"Ponto 8",latitude:-22.927,longitude:-48.434 },
-      { ordem:9,nome:"Ponto 9",latitude:-22.928,longitude:-48.436 },
-      { ordem:10,nome:"Ponto 10",latitude:-22.929,longitude:-48.438 }
+    "id": 3,
+    "nome": "Jardim Norte (fictícia)",
+    "pontos": [
+      {"nome":"1","latitude":-22.91,"longitude":-48.41},
+      {"nome":"2","latitude":-22.912,"longitude":-48.412},
+      {"nome":"3","latitude":-22.914,"longitude":-48.414},
+      {"nome":"4","latitude":-22.916,"longitude":-48.416},
+      {"nome":"5","latitude":-22.918,"longitude":-48.418},
+      {"nome":"6","latitude":-22.92,"longitude":-48.42},
+      {"nome":"7","latitude":-22.922,"longitude":-48.422},
+      {"nome":"8","latitude":-22.924,"longitude":-48.424},
+      {"nome":"9","latitude":-22.926,"longitude":-48.426},
+      {"nome":"10","latitude":-22.928,"longitude":-48.428}
     ]
   },
-  { id: 4, nome: "Vila Nova (fictícia)", pontos: [
-      { ordem:1,nome:"Ponto 1",latitude:-22.930,longitude:-48.440 },
-      { ordem:2,nome:"Ponto 2",latitude:-22.931,longitude:-48.442 },
-      { ordem:3,nome:"Ponto 3",latitude:-22.932,longitude:-48.444 },
-      { ordem:4,nome:"Ponto 4",latitude:-22.933,longitude:-48.446 },
-      { ordem:5,nome:"Ponto 5",latitude:-22.934,longitude:-48.448 },
-      { ordem:6,nome:"Ponto 6",latitude:-22.935,longitude:-48.450 },
-      { ordem:7,nome:"Ponto 7",latitude:-22.936,longitude:-48.452 },
-      { ordem:8,nome:"Ponto 8",latitude:-22.937,longitude:-48.454 },
-      { ordem:9,nome:"Ponto 9",latitude:-22.938,longitude:-48.456 },
-      { ordem:10,nome:"Ponto 10",latitude:-22.939,longitude:-48.458 }
-  ]},
-  { id: 5, nome: "São José (fictícia)", pontos: [
-      { ordem:1,nome:"Ponto 1",latitude:-22.940,longitude:-48.460 },
-      { ordem:2,nome:"Ponto 2",latitude:-22.941,longitude:-48.462 },
-      { ordem:3,nome:"Ponto 3",latitude:-22.942,longitude:-48.464 },
-      { ordem:4,nome:"Ponto 4",latitude:-22.943,longitude:-48.466 },
-      { ordem:5,nome:"Ponto 5",latitude:-22.944,longitude:-48.468 },
-      { ordem:6,nome:"Ponto 6",latitude:-22.945,longitude:-48.470 },
-      { ordem:7,nome:"Ponto 7",latitude:-22.946,longitude:-48.472 },
-      { ordem:8,nome:"Ponto 8",latitude:-22.947,longitude:-48.474 },
-      { ordem:9,nome:"Ponto 9",latitude:-22.948,longitude:-48.476 },
-      { ordem:10,nome:"Ponto 10",latitude:-22.949,longitude:-48.478 }
-  ]}
-];
+  {
+    "id": 4,
+    "nome": "Zona Leste (fictícia)",
+    "pontos": [
+      {"nome":"1","latitude":-22.95,"longitude":-48.41},
+      {"nome":"2","latitude":-22.948,"longitude":-48.412},
+      {"nome":"3","latitude":-22.946,"longitude":-48.414},
+      {"nome":"4","latitude":-22.944,"longitude":-48.416},
+      {"nome":"5","latitude":-22.942,"longitude":-48.418},
+      {"nome":"6","latitude":-22.94,"longitude":-48.42},
+      {"nome":"7","latitude":-22.938,"longitude":-48.422},
+      {"nome":"8","latitude":-22.936,"longitude":-48.424},
+      {"nome":"9","latitude":-22.934,"longitude":-48.426},
+      {"nome":"10","latitude":-22.932,"longitude":-48.428}
+    ]
+  },
+  {
+    "id": 5,
+    "nome": "Anel Sul (fictícia)",
+    "pontos": [
+      {"nome":"1","latitude":-22.98,"longitude":-48.45},
+      {"nome":"2","latitude":-22.978,"longitude":-48.448},
+      {"nome":"3","latitude":-22.976,"longitude":-48.446},
+      {"nome":"4","latitude":-22.974,"longitude":-48.444},
+      {"nome":"5","latitude":-22.972,"longitude":-48.442},
+      {"nome":"6","latitude":-22.97,"longitude":-48.44},
+      {"nome":"7","latitude":-22.968,"longitude":-48.438},
+      {"nome":"8","latitude":-22.966,"longitude":-48.436},
+      {"nome":"9","latitude":-22.964,"longitude":-48.434},
+      {"nome":"10","latitude":-22.962,"longitude":-48.432}
+    ]
+  }
+]
