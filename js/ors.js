b4ea503afb30cd5f06294a2478e788dc25fb67f8
ofilipelourencodesigner-cloud <ofilipelourencodesigner@gmail.com>
2025-10-06 11:33:14 -0300

    Create ors.js

---
+++ js/ors.js
@@ -0,0 +1,24 @@
+export async function fetchRouteGeoJSON({ points, orsKey }) {
+  const coordinates = points.map(p => [p.lon, p.lat]); // [lon, lat]
+
+  const res = await fetch('https://api.openrouteservice.org/v2/directions/driving-car/geojson', {
+    method: 'POST',
+    headers: {
+      'Authorization': orsKey,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json, application/geo+json'
+    },
+    body: JSON.stringify({
+      coordinates,
+      instructions: true,
+      instructions_format: 'text',
+      language: 'pt'
+    })
+  });
+
+  if (!res.ok) {
+    const txt = await res.text();
+    throw new Error(`ORS ${res.status}: ${txt}`);
+  }
+  return res.json();
+}
